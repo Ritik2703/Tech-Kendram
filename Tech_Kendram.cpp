215018717e7e20260821073e6fa5365adf52b51e
RITIK DAGAR <47457948+Ritik2703@users.noreply.github.com>
2022-05-29 16:24:17 +0530

    Welcome to tech kendram

---
+++ Tech_Kendram.cpp
@@ -0,0 +1,31 @@
+/*
+  radhe radhe shyam milade
+  hare krishna hare krishna
+  krishna krishna hare hare
+  hare rama hare rama
+  rama rama hare hare
+*/
+
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define loop(i,n) for(int i=0;i<n;i++)
+using namespace std;
+class radhe{
+  public:
+  radhe(ll t)
+  {
+          while(t--)
+          {
+              cout<<"Welcome to tech kendram";
+		  }
+  }
+};
+int main() {
+	fast
+	ll t; cin>>t;
+	radhe r(t);
+	return 0;
+}
+
+
